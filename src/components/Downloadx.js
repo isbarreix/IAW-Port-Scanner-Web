dde963d756d021b4d6d27c0fbf43f52b7391a672
ISB\ISB <isbarraeis@gmail.com>
2018-12-17 09:04:15 -0300

    Se acomodó seccion download

--- src/components/Downloadx.js
+++ src/components/Downloadx.js
@@ -7,13 +7,13 @@ class Downloadx extends Component {
             <div className="container">
                 <div className="row d-md-flex text-center wow fadeIn">
                     <div className="col-md-6 offset-md-3 col-sm-10 offset-sm-1 col-xs-12">
-                        <h5 className="text-primary">Download Our Mobile App</h5>
+                        <h5 className="text-primary">Download</h5>
                         <p className="mt-4">
-							Descargá gratis SPYGLASS para escanear direcciones IP y puertos a la mayor velocidad posible.
+							Descargá gratis SPYGLASS para escanear direcciones IP y puertos a la mayor velocidad posible. Accede al repositorio de la aplicación.
                         </p>
                         <p className="mt-5">
-                            <a href="#main" className="mr-2 page-scroll"><img src="img/google-play.png" className="store-img" alt="androidownload"/></a>
-                            <a href="#main" className="page-scroll"><img src="img/apple_store.png" className="store-img" alt="appledownload"/> </a>
+                            <a href="https://github.com/isbarreix/IAW-Port-Scanner" className="mr-2 page-scroll"><img src="img/repositorio.png" className="store-img" alt="androidownload"/></a>
+                         
                         </p>
                     </div>
                 </div>
